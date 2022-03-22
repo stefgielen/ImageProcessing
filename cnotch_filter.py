b1224bf5945d66ee62bcc2dfc514e79722866ac9
NathanSlembrouck <nathan.slembrouck@student.kuleuven.be>
2022-03-22 16:41:29 +0100

    cnotch
    cnotch filter werkt nu volledig

--- cnotch_filter.py
+++ cnotch_filter.py
@@ -5,6 +5,16 @@ from matplotlib import pyplot as plt
 import cv2
 from skimage.morphology import square, dilation
 from basic_filters import lpcfilter, hpcfilter, plot_filter
+import cv2 as cv
+import matplotlib.pyplot as plt
+import pylab as p
+from skimage import io
+from skimage import util
+import imageio as iio
+import numpy as np
+from cband_filter import cband_filter
+from skimage.morphology import square, dilation
+from periodic_noise import periodic_noise
 
 
 def cnotch_filter(shape=None, centers=None, ftype='ideal', reject=True, D0=0, n=1):
@@ -18,43 +28,51 @@ def cnotch_filter(shape=None, centers=None, ftype='ideal', reject=True, D0=0, n=
     n: order(s) of the butterworth filter notches
     """
     H1 = hpcfilter(shape, D0=D0, n=n, ftype=ftype, center=centers[0])
-    H2 = hpcfilter(shape, D0=D0, n=n, ftype=ftype, center=(centers[0][0]*(-1),centers[1][0]*(-1)))
+    H2 = hpcfilter(shape, D0=D0, n=n, ftype=ftype, center=(centers[0][0]*(-1),centers[0][1]*(-1)))
     H = H1*H2
-    if(ftype == 'butterworth'):
-        for element in centers[1:]:
-            H_single_pos = hpcfilter(shape,D0=D0,n=n,ftype=ftype, center=element)
-            H_single_neg = hpcfilter(shape, D0=D0,n=n, ftype=ftype, center=(element[0]*(-1),element[1]*(-1)))
-            H = H*H_single_pos*H_single_neg
-        return H
+    for element in centers[1:]:
+        H_single_pos = hpcfilter(shape,D0=D0,n=n,ftype=ftype, center=element)
+        H_single_neg = hpcfilter(shape, D0=D0,n=n, ftype=ftype, center=(element[0]*(-1),element[1]*(-1)))
+        H = H*H_single_pos*H_single_neg
+    return H
 
 """
 functie testen
 """
-from periodic_noise import periodic_noise
-img = io.imread('./imgs/imgs/SEMintegratedcircuit.jpg')
+"""
+img = iio.imread('./imgs/imgs/SEMintegratedcircuit.jpg')
+img = util.img_as_float(img)
+
 M, N = img.shape
 thetas = np.array([0,60,120])
 
+
 r, R = periodic_noise(img.shape, thetas)
 g = img + r/3
+plt.figure();plt.axis('off');plt.imshow(g, cmap='gray')
+plt.show()
+
 G = np.fft.fftshift(np.fft.fft2(g))
 Gd = dilation(np.log(1+np.abs(G)), square(3))
+plt.figure();plt.axis('off');plt.imshow(Gd, cmap='gray')
+
 xy = np.array(plt.ginput(-1, show_clicks=True))
 plt.show()
 rc = xy[:,::-1]
 
-center = np.array(img.shape)//2
-rc = rc - center
-H = cnotch_filter(img.shape,ftype='butterworth',reject=True,D0=20, centers=[(20,30),(30,40),(100,100),(0,200)])
+center=np.array(img.shape)//2
+rc = rc-center
+D0 = (np.sum(rc**2,axis=1)**0.5).mean()
+H = cnotch_filter(img.shape,rc, ftype='ideal',reject=True,D0=3, n=5)
 G2 = G*H
-g2 = np.real(ifft2(np.fft.fftshift(G2)))
+g2 = np.real(np.fft.ifft2(np.fft.fftshift(G2)))
 
-#plt.imshow(H)
-plt.subplot(411);plt.axis('off');plt.imshow(g)
+plt.plot();plt.axis('off');plt.imshow(g)
 plt.show()
-plt.subplot(412);plt.axis('off');plt.imshow(Gd)
+plt.plot();plt.axis('off');plt.imshow(Gd)
 plt.show()
-plt.subplot(413);plt.axis('off');plt.imshow(H)
+plt.plot();plt.axis('off');plt.imshow(H)
 plt.show()
-plt.subplot(414);plt.axis('off');plt.imshow(g2)
+plt.plot();plt.axis('off');plt.imshow(g2)
 plt.show()
+"""
