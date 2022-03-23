2ef7dfe4c525fb78bdba7936c1615713ae3b0ba1
NathanSlembrouck <nathan.slembrouck@student.kuleuven.be>
2022-03-23 19:47:43 +0100

    Update rnotch_filter.py
    Alle types werken, enkel het centrum van de filters is misschien nog niet af. Dit moeten we vragen aan Kevin.

--- rnotch_filter.py
+++ rnotch_filter.py
@@ -3,56 +3,9 @@ from scipy import ndimage as ndi
 import imageio
 import skimage
 from matplotlib import pyplot as plt
+from basic_filters import plot_filter
 
 def rnotch_filter(shape, D0=0, angle=0, ftype='ideal', reject=True, W=1, n=1):
-    """
-    :param shape: shape of the filter
-    :param D0: start (from center) of rectangular notch(es) (till image edge). (shape=(K,))
-    :param angle: angle (in degree) of notch with x-axis
-    :param ftype: type of the filter:'ideal', 'gaussian', or 'butterworth'
-    :param reject: True or False (pass filter)
-    :param W: notch width(s)
-    :param n: order(s) of the butterworth filter notches
-    :return: rectangular notch filter in the frequency domain
-    """
-    r,c = shape
-    D0 = np.atleast_1d(D0)
-    K = D0.size
-
-    angle = np.ones((K,))*angle
-    W = np.ones((K,))*W
-    n = np.ones((K,))*n
-
-    d = 2*np.ceil(np.sqrt((r/2)**2 + (c/2)**2))+1
-    R,C = [x-d//2 for x in np.ogrid[:d,:d]]
-
-    H = []
-    for k in range(K):
-        if (ftype == 'ideal'):
-            #TODO: code for horizontal ideal filter
-            Hk = np.zeros(shape)
-
-        """
-        elif (ftype=='gaussian'):
-            #TODO: code for horizontal gaussian filter
-        elif (ftype == 'butterworth'):
-            #TODO: code for horizontal butterworth filter
-        """
-        Hk = ndi.rotate(Hk,angle=angle[k],mode='reflect',reshape=False,order=1)
-
-        Hk = Hk[int(d//2-r//2):int(d//2 + r//2 + r%2), \
-             int(d//2 - c//2):int(d//2 + c//2 + c%2)]
-        H.append(Hk)
-
-    H = np.array(H).prod(axis=0)
-
-    if (reject == False):
-        H = np.abs(1-H)
-    print(H)
-    return H
-
-
-def rnotch_filter_test(shape, D0=0, angle=0, ftype='ideal', reject=True, W=1, n=1):
     """
     :param shape: shape of the filter
     :param D0: start (from center) of rectangular notch(es) (till image edge). (shape=(K,))
@@ -76,22 +29,25 @@ def rnotch_filter_test(shape, D0=0, angle=0, ftype='ideal', reject=True, W=1, n=
 
     H = []
     Hk = []
+    Hk = np.zeros([int(d), int(d)])
+    center = int(d) // 2
+    width = int(W)
+
     for k in range(K):
         if (ftype == 'ideal'):
-            # TODO: code for horizontal ideal filter
-            Hk = np.zeros([int(d),int(d)])
-            center = int(d)//2
-            width = int(W)
             Hk[center-width: center+width+1,0:center-D0[k]+1] = 1.0
             Hk[center-width: center+width+1,center+D0[k]:] = 1.0
             Hk = 1-Hk
-            #print(Hk)
-        """
         elif (ftype=='gaussian'):
-            #TODO: code for horizontal gaussian filter
+            Hk[:,0:center-D0[k]+1] = np.exp(-R ** 2 / (2 * D0 ** 2))
+            Hk[:,center + D0[k]:] = np.exp(-R ** 2 / (2 * D0 ** 2))
+            Hk = 1 - Hk
+
         elif (ftype == 'butterworth'):
-            #TODO: code for horizontal butterworth filter
-        """
+            Hk[:, 0:center - D0[k] + 1] = 1 / (1 + (R / D0) ** (2 * n))
+            Hk[:, center + D0[k]:] = 1 / (1 + (R / D0) ** (2 * n))
+            Hk = 1 - Hk
+
         Hk = ndi.rotate(Hk, angle=angle[k], mode='reflect', reshape=False, order=1)
 
         Hk = Hk[int(d // 2 - r // 2):int(d // 2 + r // 2 + r % 2), \
@@ -103,40 +59,53 @@ def rnotch_filter_test(shape, D0=0, angle=0, ftype='ideal', reject=True, W=1, n=
     if (reject == False):
         H = np.abs(1 - H)
     return H
-
-#test_filter = rnotch_filter_test((11,11), D0=[2],W=1,angle=90)
-#print(test_filter)
-
+"""
+functie testen
+"""
+"""
 img = imageio.imread('./imgs/imgs/saturn_rings.tif')
 img = skimage.util.img_as_float(img)
 F = np.fft.fftshift(np.fft.fft2(img))
-#plt.imshow(np.log(1+np.abs(F)))
-plt.show()
-H = rnotch_filter_test(img.shape,D0=10,ftype='ideal',W=10,angle=90)
-G = F*H
-g = np.real(np.fft.ifft2(np.fft.fftshift(G)))
 
-#fig, (ax1,ax2,ax3,ax4) = plt.subplots(nrows=4, ncols=1, figsize=(3,12), sharex=True)
-fig, ax = plt.subplots(1,4)
-ax[1].imshow(img)
-ax[2].imshow(np.log(1+np.abs(F)))
-ax[3].imshow(np.log(1+np.abs(G)))
-ax[0].imshow(g)
-plt.show()
+fig, ax = plt.subplots(3,4)
+fig.suptitle('rectangular notch filters', fontsize=16)
+ax[0,0].set_title('Noisy image')
+ax[0,1].set_title('Fourier spectrum')
+ax[0,2].set_title('Filtered Fourier spectrum')
+ax[0,3].set_title('Filtered image')
+left, width = 0, .5
+bottom, height = .25, .5
+right = left + width
+top = bottom + height
+ax[0,0].text(left, 0.5 * (bottom + top), 'ideal',
+        horizontalalignment='right',
+        verticalalignment='center',
+        rotation='vertical',
+        transform=ax[0,0].transAxes)
+ax[0,1].text(left, 0.5 * (-2 + top), 'butterworth',
+        horizontalalignment='right',
+        verticalalignment='center',
+        rotation='vertical',
+        transform=ax[0,0].transAxes)
+ax[2,0].text(left, 0.5 * (-4.5 + top), 'gaussian',
+        horizontalalignment='right',
+        verticalalignment='center',
+        rotation='vertical',
+        transform=ax[0,0].transAxes)
+for i in range(3):
+    if(i==0):
+        ftype = 'ideal'
+    elif(i==1):
+        ftype = 'butterworth'
+    else:
+        ftype = 'gaussian'
+    H = rnotch_filter(img.shape,D0=10,ftype=ftype,W=10,angle=90,n=5)
+    G = F*H
+    g = np.real(np.fft.ifft2(np.fft.fftshift(G)))
 
-"""
-ax1.imshow(img);ax1.axis('off');
-ax1.set_title('Noisy image')
-plt.show()
-ax2.imshow(img);ax2.axis('off')
-ax2.set_title('Fourier spectrum')
-plt.show()
-"""
-"""
-ax3.imshow(np.log(1+np.abs(G)));ax3.axis('off')
-ax3.set_title('Filtered fourier spectrum')
-plt.show()
-ax4.imshow(g);ax4.axis('off');
-ax4.set_title('Filterd image')
+    ax[i,0].imshow(img);ax[i,0].axis('off')
+    ax[i,1].imshow(np.log(1+np.abs(F)));ax[i,1].axis('off')
+    ax[i,2].imshow(np.log(1+np.abs(G)));ax[i,2].axis('off')
+    ax[i,3].imshow(g);ax[i,3].axis('off')
 plt.show()
 """
