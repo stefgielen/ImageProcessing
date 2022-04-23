451dcd493ffae066cb996272248cd255b7321dc2
Sofie Verschueren <verschueren.sofie@telenet.be>
2022-04-23 15:51:08 +0200

    commentaar toegevoegd

--- Piecewise_warp.py
+++ Piecewise_warp.py
@@ -10,14 +10,25 @@ from skimage.draw import polygon, rectangle_perimeter
 
 
 def add_corners(pts, image):
+    """
+    adds corner and edge points
+    :param pts: list of facial recognition points
+    :param image: image
+    :return: pts with corner points and edge points
+    """
     h, w, z = image.shape
-    #points = np.array([[0, 0], [w, 0], [0, h], [w, h], [w / 2, 0], [0, h / 2], [w, h / 2], [w / 2, h]])
-    points = np.array([[1, 1], [w-1, 1], [1, h-1], [w-1, h-1], [w / 2, 1], [1, h / 2], [w-1, h / 2], [w / 2, h-1]])
+    points = np.array([[0, 0], [w, 0], [0, h], [w, h], [w / 2, 0], [0, h / 2], [w, h / 2], [w / 2, h]])
+    #points = np.array([[1, 1], [w-1, 1], [1, h-1], [w-1, h-1], [w / 2, 1], [1, h / 2], [w-1, h / 2], [w / 2, h-1]])
     pts = np.vstack((points, pts))
     return pts
 
 
 def get_points(image):
+    """
+    Uses the shape predictor 68 face landmarks to find the face recognition points in an image
+    :param image:image
+    :return: list of array with points (for each face in the image)
+    """
     p = "./shape_predictor_68_face_landmarks.dat"
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor(p)
@@ -33,6 +44,12 @@ def get_points(image):
 
 
 def get_triangular_mesh_t(image, pts):
+    """
+
+    :param image: image (only used when plotting the result)
+    :param pts: points determined by the face landmarks predictor
+    :return: points of triangles that devide the image in pieces
+    """
     tris = Delaunay(pts)
 
     """fig, (ax) = plt.subplots(nrows=1, ncols=1)
@@ -45,6 +62,9 @@ def get_triangular_mesh_t(image, pts):
 
 
 def get_max_shape(image1, image2_shape):
+    """
+    :return: the maximum width, heigth and the third index
+    """
     w1, h1, z1 = image1.shape
     w2, h2, z2 = image2_shape
     w = max(w1, w2)
@@ -53,6 +73,10 @@ def get_max_shape(image1, image2_shape):
 
 
 def get_bounding_box(t1):
+    """
+    :param t1: three points that form a triangle on the image
+    :return: rectangle that surrounds the triangle
+    """
     start_x = min(t1[:, 0])
     start_y = min(t1[:, 1])
     w = max(t1[:, 0]) - min(t1[:, 0])
@@ -62,12 +86,25 @@ def get_bounding_box(t1):
 
 
 def warp_triangle(image, bb, M, output_shape):
+    """
+    :param image: Image that is going to be warped
+    :param bb: bounding box, selects section of image
+    :param M: Transformation
+    :param output_shape: bounding box of output shape
+    :return: part of the image selected by the boundingbox transformed with the transformation M to the shape of output_shape
+    """
     sub_image = image[int(bb[1]):int(bb[1] + bb[3]), int(bb[0]):int(bb[0] + bb[2])]
     wt = warp(sub_image, M, output_shape=output_shape)
     return wt
 
 
 def get_triangle_mask(tm, bbm, output_shape):
+    """
+    :param tm: points of triangle
+    :param bbm: points of bounding box surrounding the triangle
+    :param output_shape:
+    :return: Mask of zero's with the measurements of output_shape  and ones in the shape of the triangle
+    """
     tm = tm.astype(int)
     rr, cc = polygon(tm[:, 1] - int(bbm[1]), tm[:, 0] - int(bbm[0]), output_shape)
     mask = np.zeros([output_shape[0], output_shape[1], 3])
@@ -76,6 +113,14 @@ def get_triangle_mask(tm, bbm, output_shape):
 
 
 def warp_image(im1, pts, tris, ptsm, im2_shape):
+    """
+    :param im1: image that need warping
+    :param pts: face landmark points
+    :param tris: array of triangles dividing image 1 in pieces
+    :param ptsm: points defined by intermediate key points
+    :param im2_shape: shape of second image
+    :return: warped image
+    """
     max_shape_of_im1_and_im2 = get_max_shape(im1, im2_shape)
     warped = np.zeros(max_shape_of_im1_and_im2)
 
